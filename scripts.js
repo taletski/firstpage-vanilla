1d0ee6c1c4cd44348de00c34910e91507fd54185
Kirill Taletski <kirill.taletski@gmail.com>
2020-03-21 03:53:40 -0700

    implemented the gallery

--- scripts.js
+++ scripts.js
@@ -5,12 +5,13 @@ function currentDiv(n) {
 	showDivs(slideIndex = n);
 }
 
-var test_external = []
+var test;
 
 function showDivs(n) {
 	var i;
 	var images = document.getElementsByClassName("slideshow__display-image");
-	test_external = images;
+	var dots = document.getElementsByClassName("slideshow__nav-circle");
+	test = dots;
 
 	if (n > images.length) {slideIndex = 1};
 	if (n < 1) {slideIndex = images.length};
@@ -19,10 +20,13 @@ function showDivs(n) {
 		images[i].style.display = "none";
 	}
 	images[slideIndex - 1].style.display = "block";
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className.baseVal = dots[i].className.baseVal.replace(" slideshow__nav-circle_white", "");
+		/* WARNING: in this case dots refer to svg objects, therefore, 
+		.className returns SVGAnimatedString instead of a simple string.
+		THIS IS NOT SUPPORTED by Chrome for Android and by IE.
+		This also requires to refer to modify the baseVal of the string*/
+	}
+	dots[slideIndex - 1].className.baseVal += " slideshow__nav-circle_white";
 }
 
-
-
-// document.addEventListener('load', {
-// 	var images = document.getElementsByClassName("slideshow__display-image");
-// });
