b7406b8d030126b2e7400485c5fcf2eed983bdf3
Kirill Taletski <kirill.taletski@gmail.com>
2020-03-20 23:30:33 -0700

    implemented onclick switching between the pictures in the gallery (not styled yet)

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+document.addEventListener('load', showDivs(slideIndex));
 
 function currentDiv(n) {
 	showDivs(slideIndex = n);
 }
 
+var test_external = []
+
 function showDivs(n) {
+	var i;
+	var images = document.getElementsByClassName("slideshow__display-image");
+	test_external = images;
+
+	if (n > images.length) {slideIndex = 1};
+	if (n < 1) {slideIndex = images.length};
 	
+	for (i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
+	}
+	images[slideIndex - 1].style.display = "block";
 }
+
+
+
+// document.addEventListener('load', {
+// 	var images = document.getElementsByClassName("slideshow__display-image");
+// });
