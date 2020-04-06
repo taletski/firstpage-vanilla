886fbf09190feff9748d3b85cc61f115ce566749
Kirill Taletski <kirill.taletski@gmail.com>
2020-04-06 19:31:27 +0300

    Latest Works slider: add a visibility manager for the slider controls

--- slider.js
+++ slider.js
@@ -3,59 +3,58 @@
 var multiItemSlider = (function () {
 	return function(selector) {
 		var 
-			_mainElement = document.querySelector(selector), // main element of the block
+			_mainElement = document.querySelector(selector),
 			_sliderWrapper = _mainElement.querySelector('.slider__container'),
 			_sliderItems = _mainElement.querySelectorAll('.slider__item'),
 			_sliderControls = _mainElement.querySelectorAll('.slider__control'),
 			_sliderControlLeft = _mainElement.querySelector('.slider__control_left'),
 			_sliderControlRight = _mainElement.querySelector('.slider__control_right'),
-			// _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width),
-			// _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width),
 
 			_numOfVisibleItems = 4,
 			_positionLeftItem = 0,
 			_transform = 0,
-			_step = 1. / _numOfVisibleItems * 100,
-			_items = [];
-
-		_sliderItems.forEach(function (item, index) {
-			_items.push({item: item, position: index, transform: 0});
-		});
+			_step = 1. / _numOfVisibleItems * 100;
 
 		var position = {
 			getMin: 0,
-			getMax: _items.length - 1,
+			getMax: _sliderItems.length - 1,
+		}
+
+		var _manageControlsVisibility = function () {
+			_sliderControls.forEach(function (item) {
+				if (!item.classList.contains('slider__control_show')) {
+					item.classList.add('slider__control_show');
+					console.log('refresh!')
+				}
+			});
+			if (_sliderControlRight.classList.contains('slider__control_show') && 
+				(_positionLeftItem + _numOfVisibleItems - 1) >= position.getMax) {
+				_sliderControlRight.classList.remove('slider__control_show');
+			}
+			if (_sliderControlLeft.classList.contains('slider__control_show') && 
+				_positionLeftItem <= position.getMin) {
+				_sliderControlLeft.classList.remove('slider__control_show');
+			}
 		}
 
 		var _transformItem = function (direction) {
 			console.log('Called _transformItem: ' + direction)
 			switch(direction) {
 				case "right":
-					console.log('moving right')
 					if ((_positionLeftItem + _numOfVisibleItems - 1) >= position.getMax) {
 						return;
-					}
-					if (!_sliderControlRight.classList.contains("slider__control_show")){
-						_sliderControl.classList.add("slider__control_show");
-					}
-					if (_sliderControlRight.classList.contains("slider__control_show") && 
-						(_positionLeftItem + _numOfVisibleItems - 1) >= position.getMax) {
-						_positionLeftItem.classList.remove("slider__control_show");
 					}
 					_positionLeftItem++;
 					_transform -= _step;
 					break;
 				case 'left':
-					if (_positionLeftItem <= position.getMin) {
+					console.log('moving left');
+					if ((_positionLeftItem) <= position.getMin) {
 						return;
 					}
-					if (!_sliderControlLeft.classList.contains("slider__control_show")) {
-						_sliderControlLeft.classList.add("slider__control_show");
-					}
-					if (_sliderControlLeft.classList.contains("slider__control_show") && 
-						_positionLeftItem <= position.getMin) {
-						_sliderControlLeft.classList.remove("slider__control_show");
-					}
+					// if (!_sliderControlLeft.classList.contains('slider__control_show')) {
+					// 	_sliderControlLeft.classList.add('slider__control_show');
+					// }
 					_positionLeftItem--;
 					_transform += _step;
 					break;
@@ -64,6 +63,7 @@ var multiItemSlider = (function () {
 					break;
 			}
 			_sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+			_manageControlsVisibility();
 		}
 
 		var _controlClick = function (e) {
@@ -82,15 +82,6 @@ var multiItemSlider = (function () {
 		}
 
 		_setUpListeners();
-
-		return {
-			right: function () {
-				_transformItem('right');
-			},
-			left: function () {
-				_transformItem('left');
-			}
-		}
 
 	}
 }());
